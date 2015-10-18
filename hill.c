92701f413946a1e3fe7af45f02201caaf20f574e
Jose Antnonio García del Saz <plyskor@gmail.com>
2015-10-18 23:15:27 +0200

    skelton

--- hill.c
+++ hill.c
@@ -5,5 +5,17 @@
 
 
 int main (int argc,char *argv[]) {
+    int m=0, n=0;
+    FILE *fmatrix, *fin, *fout;
+    if(argc != 12){
+        printf("El programa ha de ser invocado asi: hill [-C|-D] [-m |Zm|] [-n Nk] [-k filematriz] [-i infile] [-o outfile]\n");
+        return(0);
+    }
+    m=atoi(argv[3]);
+    n=atoi(argv[5]);
+    if((n<0)||(m<0)){
+        printf("Valores no validos para m o n");
+        return(0);
+    }
     return(0);
 }
