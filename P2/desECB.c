c614dc26ccc85999c47eb38c0e149e8e5236db30
Jose Antnonio García del Saz <plyskor@gmail.com>
2015-11-16 18:35:25 +0100

    funciona leeClave

--- P2/desECB.c
+++ P2/desECB.c
@@ -13,13 +13,10 @@
 /*
  * 
  */
-void generaClaveRandom64(unsigned char *k){
-    int i =0;
+
+void printClave(unsigned char *k){
+    int i;
     if(!k)return;
-    srand(time(NULL));
-    for(i=0;i<8;i++){
-        k[i]=rand();
-    }
     printf("Su clave generada aleatoriamente en hexadecimal es:");
     for(i=0;i<8;i++){
         printf("%x",k[i]);
@@ -27,9 +24,25 @@ void generaClaveRandom64(unsigned char *k){
     printf("\n");
     return;
 }
+void generaClaveRandom64(unsigned char *k){
+    int i =0;
+    if(!k)return;
+    srand(time(NULL));
+    for(i=0;i<8;i++){
+        k[i]=rand();
+    }
+    printClave(k);
+    return;
+}
+void leeClave(const char * input,unsigned char *k){
+    if(!input||!k)return;
+    int i,j=0;
+    sscanf(input,"%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx",&k[0],&k[1],&k[2],&k[3],&k[4],&k[5],&k[6],&k[7]); 
+    return;
+}
 int main(int argc, char** argv) {
     /*ARGUMENTOS*/
-    if(argc!=7&&argc!=9){
+    if(argc<2){
         printf("El programa se debe ejecutar asi: desECB [-C|-D -k clave] [-S s] [-i inputFile] [-o outputFile]\n");
         return -1;
     }
@@ -39,8 +52,26 @@ int main(int argc, char** argv) {
     /*MEMORIA*/
     key= (unsigned char*)malloc(sizeof(unsigned char)*8);
     if(!key) return -1;
-    /*Generamos la clave aleatoria*/
-    generaClaveRandom64(key);
+    /*CASO CIFRADO*/
+    if(!strcmp(argv[1],"-C")){ 
+        if(argc!=7){
+            printf("El programa se debe ejecutar asi: desECB [-C|-D -k clave] [-S s] [-i inputFile] [-o outputFile]\n");
+            return -1;
+        }
+        /*Generamos clave aleatoria*/
+        generaClaveRandom64(key);
+    }
+    /*CASO DESCIFRADO*/
+    if(!strcmp(argv[1],"-D")){
+       /*Generamos la clave aleatoria*/ 
+       if(argc!=9){
+        printf("El programa se debe ejecutar asi: desECB [-C|-D -k clave] [-S s] [-i inputFile] [-o outputFile]\n");
+        return -1;
+        }
+       leeClave(argv[3],key);
+       printClave(key);
+    }
+    
     /*LIBERAR MEMORIA*/
     free(key);
     return (EXIT_SUCCESS);
